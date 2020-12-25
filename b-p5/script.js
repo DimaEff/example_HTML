8b90e90b545f8d378d09f61b98578e6db3452534
DimaEff <fominienkovd@users.noreply.github.com>
2020-12-25 22:40:35 +0300

    New test design

---
+++ b-p5/script.js
@@ -0,0 +1,6 @@
+$('.btn-friends').on('click', function() {
+    $('.menu-friends').toggleClass('menu-active');
+})
+$('.btn-messages').on('click', function() {
+    $('.menu-messages').toggleClass('menu-active');
+})
